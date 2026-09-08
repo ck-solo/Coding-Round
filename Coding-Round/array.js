43c7798c1d9d9eef3e29b50f5b28b48a78d175b6
Chandan Kumar <solouser019@gmail.com>
2026-09-08 19:10:12 +0530

    revise array

--- Coding-Round/array.js
+++ Coding-Round/array.js
@@ -12,16 +12,31 @@
 // console.log("No repeated element");
 
 // Find the largest and smallest element in an array.
-const nums = [5, 15, 22, 1, -15, 24];
-let smallest = nums[0];
-let largest = nums[0];
-for (let i = 1; i < nums.length; i++) {
-    if (nums[i] < smallest) {
-        smallest = nums[i];
-    }
+// const nums = [5, 15, 22, 1, -15, 24];
+// let smallest = nums[0];
+// let largest = nums[0];
+// for (let i = 1; i < nums.length; i++) {
+//     if (nums[i] < smallest) {
+//         smallest = nums[i];
+//     }
+//     if (nums[i] > largest) {
+//         largest = nums[i];
+//     }
+// }
+// console.log("Smallest:", smallest);
+// console.log("Largest:", largest);
+
+
+// find the second largest 
+const nums = [10, 5, 20, 8, 20, 15];
+let largest = -Infinity;
+let secondLargest = -Infinity;
+for (let i = 0; i < nums.length; i++) {
     if (nums[i] > largest) {
+        secondLargest = largest;
         largest = nums[i];
+    } else if (nums[i] > secondLargest && nums[i] !== largest) {
+        secondLargest = nums[i];
     }
 }
-console.log("Smallest:", smallest);
-console.log("Largest:", largest);
+console.log("Second largest:", secondLargest);
