9216719e220eab360e74f37db3d345f4a43cfe2b
Chandan Kumar <solouser019@gmail.com>
2026-09-10 19:50:36 +0530

    feat: add Pagination component for form navigation

--- React/src/Form/components/Pagination.jsx
+++ React/src/Form/components/Pagination.jsx
@@ -3,25 +3,9 @@ function Pagination({ page, totalPages, setPage }) {
 
   return (
     <div className="tm-pagination">
-      <button
-        className="tm-btn tm-btn-secondary"
-        disabled={page === 1}
-        onClick={() => setPage(page - 1)}
-      >
-        Prev
-      </button>
-
-      <span className="tm-pagination-info">
-        Page {page} of {totalPages}
-      </span>
-
-      <button
-        className="tm-btn tm-btn-secondary"
-        disabled={page === totalPages}
-        onClick={() => setPage(page + 1)}
-      >
-        Next
-      </button>
+      <button className="tm-btn tm-btn-secondary" disabled={page === 1} onClick={() => setPage(page - 1)} ></button>
+      <span className="tm-pagination-info">Page {page} of {totalPages}</span>
+      <button className="tm-btn tm-btn-secondary" disabled={page === totalPages} onClick={() => setPage(page + 1)}>Next</button>
     </div>
   );
 }
