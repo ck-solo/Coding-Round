bd257e12e40b2281a8368ab0716952fcf07713dd
Chandan Kumar <solouser019@gmail.com>
2026-09-11 19:03:20 +0530

    revise

--- React/src/Form/components/TaskSearch.jsx
+++ React/src/Form/components/TaskSearch.jsx
@@ -13,3 +13,21 @@ function TaskSearch({ search, setSearch, setPage }) {
 }
 
 export default TaskSearch;
+
+
+// revise 
+// function TaskSearch({ search, setSearch, setPage }) {
+//   function handleSearch(e) {
+//     setSearch(e.target.value);
+//     setPage(1);
+//   }
+
+//   return (
+//     <div>
+//       <input className="tm-input" type="text" placeholder="Search by title or description" value={search} onChange={handleSearch}
+//       />
+//     </div>
+//   );
+// }
+
+// export default TaskSearch;
