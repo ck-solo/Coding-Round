2e59cc004b6d1a242b0b78a92f390cac0c8d149f
Chandan Kumar <solouser019@gmail.com>
2026-09-13 19:52:43 +0530

    revise controller

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -1,6 +1,17 @@
-import listModel from "../models/list.models"
+import listModel from "../models/list.models.js"
+import jwt from 'jsonwebtoken'
 
 export async function register(req,res){
     const {email, password} = req.body
     const user = await listModel.create({email, password})
 }
+
+const token = jwt.sign({
+    email:user.email,
+    id:user._id
+},JWT_SECRET)
+
+res.status(201).json({
+    message:"User created successfully",
+    token:token
+})
